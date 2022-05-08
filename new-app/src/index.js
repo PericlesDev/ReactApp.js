a258cfae8755ae18bb037ffc2c82fabbc23b9bd3
unknown <PericlesDosSantosRamos@hotmail.com>
2022-05-08 19:29:48 -0300

    Renderização

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,27 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import Button from './Button'
-import ComponentA from './ComponentA'
-import ComponentB from './ComponentB'
+import React, { Fragment } from "react"
+import ReactDOM from "react-dom"
 import "./style.css"
 
-function soma(a, b) {
-  alert(a + b)
-}
+ const element = 'Digital Innovation'
+ const element2 = <h1>Olá, Mundo!</h1>
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-      <Button onClick={() => soma(10, 20)} name="Bruno Carneiro" />
-      <ComponentA>
-      <ComponentB>
-      <Button onClick={() => soma(40, 20)} name="Aline Carneiro" />
-      </ComponentB>
-      </ComponentA>
+    <div>
+    {element}
+    {element2}
     </div>
   )
 }
 
 const rootElement = document.getElementById("root")
-ReactDOM.render(<app />, rootElement)
+ReactDOM.render(<App />, rootElement)
