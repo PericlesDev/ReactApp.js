27a27f49e9c4fefad29bd186a2c8e1b4cb01da29
unknown <PericlesDosSantosRamos@hotmail.com>
2022-05-08 19:13:48 -0300

    Components

---
+++ new-app/src/ComponentB.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ComponenteB(props) {
+    return (
+        <div>
+            <div>Componente B =D {props.children} </div>
+        </div>
+    )
+}
+
+export default ComponenteB
